c2f5b38beeb3f335c1df0d5b66af070af29216c7
WitoldWzgarda <113769562+WitoldWzgarda@users.noreply.github.com>
2022-10-08 13:18:45 +0200

    Lekcja 4
    - PickUpy

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,7 +9,11 @@ public class GameManager : MonoBehaviour
     bool gamePaused = false;
     bool endGame = false;
     bool win = false;
+    public int points = 0;
 
+    public int redKey = 0;
+    public int greenKey = 0;
+    public int goldKey = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -30,6 +34,7 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         PauseCheck();
+        PickUpCheck();
     }
 
     void Stopper()
@@ -84,6 +89,47 @@ public class GameManager : MonoBehaviour
         } else
         {
             Debug.Log("You lose!!! Reload?");
+        }
+    }
+
+    public void AddPoints(int point)
+    {
+        points += point;
+    }
+
+    public void AddTime(int addTime)
+    {
+        timeToEnd += addTime;
+    }
+
+    public void FreezTime(int freez)
+    {
+        CancelInvoke("Stopper");
+        InvokeRepeating("Stopper", freez, 1);
+    }
+
+    public void AddKey(KeyColor color)
+    {
+        if (color == KeyColor.Gold)
+        {
+            goldKey++;
+        } else if(color == KeyColor.Green)
+        {
+            greenKey++;
+        }
+        else if (color == KeyColor.Red)
+        {
+            redKey++;
+        }
+    }
+
+    void PickUpCheck()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            Debug.Log("Actual time: " + timeToEnd);
+            Debug.Log("Key red: " + redKey + " green: " + greenKey + " gold: " + goldKey);
+            Debug.Log("Points: " + points);
         }
     }
 }
