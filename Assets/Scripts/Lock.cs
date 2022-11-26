e33a6b59543a0df13a92dd6aed7386dceacbb15d
WitoldWzgarda <113769562+WitoldWzgarda@users.noreply.github.com>
2022-11-26 13:19:09 +0100

    Lekcja 10
    - animacje
    - zamek
    - skrypty do animacji

---
+++ Assets/Scripts/Lock.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lock : MonoBehaviour
+{
+    public Door[] doors;
+    public KeyColor myColor;
+    bool iCanOpen = false;
+    bool locked = false;
+    Animator key;
+    // Start is called before the first frame update
+    void Start()
+    {
+        key = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && iCanOpen && !locked)
+        {
+            key.SetBool("useKey", CheckTheKey());
+        }
+    }
+
+    public void UseKey()
+    {
+        foreach (Door door in doors)
+        {
+            door.OpenClose();
+        }
+    }
+
+    public bool CheckTheKey()
+    {
+        if (GameManager.gameManager.redKey > 0 && myColor == KeyColor.Red)
+        {
+            GameManager.gameManager.redKey--;
+            locked = true;
+            return true;
+        }
+        else if (GameManager.gameManager.greenKey > 0 && myColor == KeyColor.Green)
+        {
+            GameManager.gameManager.greenKey--;
+            locked = true;
+            return true;
+        }
+        else if (GameManager.gameManager.goldKey > 0 && myColor == KeyColor.Gold)
+        {
+            GameManager.gameManager.goldKey--;
+            locked = true;
+            return true;
+        }
+        else
+        {
+            Debug.Log("Nie masz klucza");
+            return false;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            iCanOpen = true;
+            Debug.Log("You can Use lock");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            iCanOpen = false;
+            Debug.Log("You can not Use lock");
+        }
+    }
+}
